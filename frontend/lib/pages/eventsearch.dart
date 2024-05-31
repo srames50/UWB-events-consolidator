c1875d0320d61e00f5e8f444b1cad202f98f9847
AkshatJain2004 <85363596+AkshatJain2004@users.noreply.github.com>
2024-05-31 13:58:46 +0300

    Commenting eventsearch.dart

--- frontend/lib/pages/eventsearch.dart
+++ frontend/lib/pages/eventsearch.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:frontend/pages/event.dart'; // Import EventPage here
 
+// This allows the user to search for events from a predefined list and navigate to the event details page.
 class EventSearchPage extends StatefulWidget {
   @override
   _EventSearchPageState createState() => _EventSearchPageState();
@@ -15,7 +16,7 @@ class _EventSearchPageState extends State<EventSearchPage> {
     'Event 4',
     'Event 5'
   ];
-  List<String> _filteredEvents = [];
+  List<String> _filteredEvents = []; // List to store filtered events based on the search query
 
   @override
   void initState() {
@@ -23,16 +24,18 @@ class _EventSearchPageState extends State<EventSearchPage> {
     _filteredEvents = _allEvents;
   }
 
+  // Method to filter events based on the search query
   void _filterEvents(String query) {
     List<String> filtered = _allEvents
         .where((event) => event.toLowerCase().contains(query.toLowerCase()))
         .toList();
     setState(() {
-      _filteredEvents = filtered;
+      _filteredEvents = filtered; // Update the filtered events list
     });
   }
 
   @override
+  // Builds the UI of the Event Search page
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
@@ -42,21 +45,23 @@ class _EventSearchPageState extends State<EventSearchPage> {
         padding: const EdgeInsets.all(8.0),
         child: Column(
           children: <Widget>[
+            // Search input field
             TextField(
               controller: _searchController,
               decoration: InputDecoration(
                 labelText: 'Search',
                 prefixIcon: Icon(Icons.search),
               ),
-              onChanged: _filterEvents,
+              onChanged: _filterEvents, // Call the _filterEvents method to update the filtered events upon a change in the search field
             ),
             Expanded(
               child: ListView.builder(
-                itemCount: _filteredEvents.length,
+                itemCount: _filteredEvents.length, // Number of filtered events
                 itemBuilder: (context, index) {
                   return ListTile(
                     title: Text(_filteredEvents[index]),
                     onTap: () {
+                      // Navigate to the EventPage when an event is tapped
                       Navigator.push(
                         context,
                         MaterialPageRoute(
